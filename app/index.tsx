84d472a15a8426e1a3612bb137b920000a22459e
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2024-08-15 23:31:53 +0000

    avance 1

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center
     bg-white">
-      <Text className="text-3xl">AORA ADSO</Text>
+      <Text className="text-3xl font-pblack">AORA ADSO</Text>
       <StatusBar style="auto" />
       <Link href='/profile' style={{color: 'blue'}}>Go to profile</Link>
     </View>
